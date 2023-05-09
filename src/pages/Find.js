18c0015363f6f9dd3a48e19ef6da225c88704827
DylanPiriou <dylan.piriou@hotmail.com>
2023-05-09 16:38:49 +0200

    advanced searchbar logic created

--- src/pages/Find.js
+++ src/pages/Find.js
@@ -7,17 +7,74 @@ import FavCard from "../components/FavCard";
 
 export default function Find() {
   const [objData, setObjData] = useState([]);
-  const [searchData, setSearchData] = useState("interstellar");
+  const [searchData, setSearchData] = useState("Interstellar");
+  const [genreId, setGenreId] = useState(null);
+  const [isGenreSelected, setIsGenreSelected] = useState(false);
 
+  // Affichage des résultats
+  useEffect(() => {
+    if (genreId) {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/discover/movie?api_key=2e0a9e72249514e45f19f77ee9930761&with_genres=${genreId}`
+        )
+        .then((res) => {
+          setObjData(res.data.results);
+          setIsGenreSelected(true);
+        });
+    } else {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie?api_key=2e0a9e72249514e45f19f77ee9930761&language=fr-FR&query=${searchData}`
+        )
+        .then((res) => {
+          setObjData(res.data.results);
+          setIsGenreSelected(false);
+        });
+    }
+  }, [genreId, searchData]);
+
+  // Gestion de la fermeture de la modale
+  useEffect(() => {
+    const handleBlur = (e) => {
+      if (searchBox.current && !searchBox.current.contains(e.target) && !e.target.matches('input[type="search"]')) {
+        setIsFocused(false);
+      }
+    }
+
+    document.addEventListener("click", handleBlur);
+
+    return () => {
+      document.removeEventListener("click", handleBlur);
+    }
+  }, [])
+
+  // Récpérer toutes les trends dans la modale
+  const [trendData, setTrendData] = useState([]);
   useEffect(() => {
     axios
       .get(
-        `https://api.themoviedb.org/3/search/movie?api_key=2e0a9e72249514e45f19f77ee9930761&language=fr-FR&query=${searchData}`
+        `https://api.themoviedb.org/3/trending/movie/week?api_key=2e0a9e72249514e45f19f77ee9930761`
       )
-      .then((res) => setObjData((res.data.results)));
-  }, [searchData]);
+      .then((res) => setTrendData(res.data.results));
+  }, []);
 
   const message = useRef(null);
+  const searchBox = useRef();
+  const [isFocused, setIsFocused] = useState(false);
+  const handleFocus = (e) => {
+    setIsFocused(true);
+  }
+
+  // Récupérer tous les genres dans la modale
+  const [genresData, setGenresData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/genre/movie/list?api_key=2e0a9e72249514e45f19f77ee9930761&language=en-FR`
+      )
+      .then((res) => setGenresData(res.data.genres));
+  }, [])
 
   return (
     <div>
@@ -25,24 +82,53 @@ export default function Find() {
       <div className="message" ref={message}></div>
       <div className="top-box">
         <h1 className="top-title">Rechercher votre film</h1>
-        <input
-          onKeyDown={(e) => {e.key === "Enter" && setSearchData(e.target.value)}}
-          type="search"
-          placeholder="ex. Interstellar"
-        />
+
+        <div className="input-wrapper">
+          <input
+            onKeyDown={(e) => { e.key === "Enter" && setSearchData(e.target.value); setGenreId(null) }}
+            onFocus={(e) => handleFocus(e)}
+            type="search"
+            placeholder={searchData}
+          />
+          {isFocused &&
+            <div className="data-box" ref={searchBox}>
+              <div className="trend-wrapper">
+                <h3>Les plus recherchés en ce moment</h3>
+                <ul>
+                  {trendData.slice(0, 10).map((film, index) => {
+                    return <li key={index} onClick={(e) => {setSearchData(e.target.textContent); setGenreId(null)}}>{film.title}</li>
+                  })}
+                </ul>
+              </div>
+              <div className="genres-wrapper">
+                <h3>Chercher par genres</h3>
+                <ul>
+                  {genresData.map((film, index) => {
+                    return <button key={index} id={film.id} onClick={(e) => setGenreId(e.target.id)}>{film.name}</button>
+                  })}
+                </ul>
+              </div>
+            </div>
+          }
+        </div>
+
       </div>
       <div className="grid-container">
-        {objData.length > 0 ?
-        (
-        objData.slice(0, 5).map((film, index) => {
-          return (
-            <FavCard key={index} film={film} message={message}/>
-          );
-        })
+        {isGenreSelected ? (
+          objData.length > 0 ? (
+            objData.slice(0, 10).map((film, index) => {
+              return <FavCard key={index} film={film} message={message} />;
+            })
+          ) : (
+            <p>Aucun résultat trouvé pour ce genre.</p>
+          )
+        ) : objData.length > 0 ? (
+          objData.slice(0, 10).map((film, index) => {
+            return <FavCard key={index} film={film} message={message} />;
+          })
         ) : (
-          <p>Nous n'avons pas trouvés de films qui correspondent à votre recherche. Veuillez réessayer.</p>
+          <p>Aucuns films trouvés. Veuillez réessayer.</p>
         )
-        
         }
       </div>
       <Footer />

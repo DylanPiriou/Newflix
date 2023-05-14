93ec04343b9cf8ade692f9f8ac5ca57d77b90a73
DylanPiriou <dylan.piriou@hotmail.com>
2023-05-14 16:11:32 +0200

    website title updated

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React from "react";
 import Find from "./pages/Find/Find";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Favoris from "./pages/Find/Favoris";
 import Film from "./pages/FilmDetails/FilmDetails";
 
 function App() {
+  const location = useLocation();
+  const name = location.pathname
+  console.log(name)
+  if(name === "/"){
+    document.title = `Home — Newflix`;
+  } else if (name === "/favoris"){
+    document.title = `Favoris — Newflix`;
+  } else if(name === "/rechercher"){
+    document.title = `Rechercher — Newflix`;
+  } else {
+    document.title = `Newflix — Pour les passionnés du cinéma`;
+  }
+
   return (
     <div className="App">
       <Routes>
